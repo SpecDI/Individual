39b61da842c180c19d80311cf774d12500e48f95
SpecDI <vlad@herghelegiu.com>
2017-10-11 09:24:06 +0100

    Chapter 7 finished

--- Book/Chapter7.hs
+++ Book/Chapter7.hs
@@ -46,7 +46,28 @@ lstCompr xs = map f resLst
            
 --2
 --a
-rtrTrue :: a -> Bool
-rtrTrue _ =  True
-all_ctm :: (a -> Bool) -> [Bool] -> Bool
-all_ctm rtrTrue x = and . map rtrTrue x
+all_ctm :: (a -> Bool) -> [a] -> Bool
+all_ctm test = foldr (&&) True . map test
+
+any_ctm :: (a -> Bool) -> [a] -> Bool
+any_ctm test = foldr (||) False . map test
+
+takeWhile_ctm :: (a -> Bool) -> [a] -> [a]
+takeWhile_ctm _ [] = []
+takeWhile_ctm test (x:xs) 
+            | test x = x : takeWhile_ctm test xs
+            | otherwise = []
+
+dropWhile_ctm :: (a -> Bool) -> [a] -> [a]
+dropWhile_ctm _ [] = []
+dropWhile_ctm test (x:xs)
+            | test x = dropWhile_ctm test xs
+            | otherwise = x : dropWhile_ctm test xs
+
+--3
+map_ctm test = foldr (\x xs -> test x : xs) []
+filter_ctm test = foldr (\x xs -> if test x then x:xs else xs) []
+
+--4
+dec2int :: [Int] -> Int
+dec2int = foldl (\x y -> 10 * x + y) 0
